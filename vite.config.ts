091b6d0d34937b9f2b7acd5c038cb7d7c14c7cd8
Kelmcclain <mcclainkel@gmail.com>
2025-01-04 02:43:26 +0300

    change base path in Vite configuration from "/incident-counter" to "/letraque"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/incident-counter",
+  base: "/letraque",
   plugins: [react()],
   resolve: {
     alias: {
